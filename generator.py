30c01789127b3474cb7ca2167d4a85b34cc4c165
Khamhuang, David <khameye19888@gmail.com>
2022-08-04 12:33:32 -0500

    page 55

--- generator.py
+++ generator.py
@@ -9,3 +9,13 @@ nouns = ['apple', 'dinosaur', 'ball', 'toaster', 'goat', 'dragon', 'hammer', 'du
 
 print('Welcome to Password Picker!')
 
+# randomly selecting a word from the lists
+adjective = random.choice(adjectives)
+noun = random.choice(nouns)
+number = random.randrange(0, 100) #randrange is a function imported from 'random', as is 'choice'
+special_char = random.choice(string.punctuation) #punctuation is a constant from 'string' module
+
+# generating a *new* password
+password = adjective + noun + str(number) + special_char # str(number) is converting the integer to a string
+
+print('Your new password is: %s' %password)
