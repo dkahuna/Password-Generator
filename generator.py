af1160d174fb4972f7313f5d37888f1b6f674c93
Khamhuang, David <khameye19888@gmail.com>
2022-08-08 21:14:39 -0500

    for loop added to present 3 passwords at a time

--- generator.py
+++ generator.py
@@ -13,18 +13,22 @@ print('Welcome to Password Picker!')
 # what if user wants a different password? -
 while True:
 
-    # randomly selecting a word from the lists
-    adjective = random.choice(adjectives)
-    noun = random.choice(nouns)
-    number = random.randrange(0, 100) #randrange is a function imported from 'random', as is 'choice'
-    special_char = random.choice(string.punctuation) #punctuation is a constant from 'string' module
+# using a for loop to show 3 different passwords at a time for user to select
+    for num in range(3):
 
-    # generating a *new* password
-    password = adjective + noun + str(number) + special_char # str(number) is converting the integer to a string
+        # randomly selecting a word from the lists
+        adjective = random.choice(adjectives)
+        noun = random.choice(nouns)
+        number = random.randrange(0, 100) #randrange is a function imported from 'random', as is 'choice'
+        special_char = random.choice(string.punctuation) #punctuation is a constant from 'string' module
 
-    print('Your new password is: %s' %password)
+        # generating a *new* password
+        password = adjective + noun + str(number) + special_char # str(number) is converting the integer to a string
+
+        print('Your new password is: %s\n' %password)
 
     # line 12 & 13
+    print('---------------------------------------------')
     response = input('Would you like another password? Type y or n: ')
     
     if response == 'n':
